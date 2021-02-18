75b157e0cbdca35236b5e9fd3d2dc3df2f6350c7
Banik1103 <jeremi.kilimnik@gmail.com>
2021-02-18 15:59:36 +0100

    Idk what this is lol

--- lib/buttons.dart
+++ lib/buttons.dart
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import 'package:flutter/material.dart';
 
 class MyButton extends StatelessWidget {
@@ -31,37 +30,3 @@ class MyButton extends StatelessWidget {
     );
   }
 }
-=======
-import 'package:flutter/material.dart';
-
-class MyButton extends StatelessWidget {
-  final color;
-  final textColor;
-  final String buttonText;
-  final buttonTapped;
-
-  MyButton({this.color, this.textColor, this.buttonText, this.buttonTapped});
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: buttonTapped,
-      child: Padding(
-        padding: const EdgeInsets.all(7.0),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(20),
-          child: Container(
-            color: color,
-            child: Center(
-              child: Text(
-                buttonText,
-                style: TextStyle(color: textColor, fontSize: 30),
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
->>>>>>> master
