9fa0eae17500158d03d04e1e1c03fd01bf0bb6d8
Banik1103 <jeremi.kilimnik@gmail.com>
2021-02-19 12:52:41 +0100

    fixing files after merging master branch into main branch

--- android/app/src/main/kotlin/com/example/calculator/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/calculator/MainActivity.kt
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
-package com.example.calculator
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
-=======
 package com.banik1103.calculator2
 
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
 }
->>>>>>> master
